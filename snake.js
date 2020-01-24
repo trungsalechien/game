d501f577d5c90b14e52ecd25105b006a29016afc
trungsalechien <60271960+trungsalechien@users.noreply.github.com>
2020-01-24 17:07:55 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -76,6 +76,48 @@ function direction(event){
     }
 }
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+var xDown = null;                                                        
+var yDown = null;  
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+}; 
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+        /* left swipe */ 
+        left.play();
+        } else {
+        /* right swipe */
+        right.play();
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+        /* up swipe */ 
+		up.play();
+        } else { 
+        /* down swipe */
+        down.play();
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 // cheack collision function
 function collision(head,array){
     for(let i = 0; i < array.length; i++){
@@ -150,8 +192,6 @@ function draw(){
 // call draw function every 100 ms
 
 let game = setInterval(draw,100);
-
-
 
 
 
